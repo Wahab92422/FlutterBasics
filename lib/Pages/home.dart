1e37dca9bc24c61c2bf12d87c50d9b86157a8d7c
Wahab92422 <abdulwahab9705@gmail.com>
2022-11-25 03:27:37 +0500

    day1 practice

---
+++ lib/Pages/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String title = 'Catalog App';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text(
+          'Its me Hi!',
+          style: TextStyle(
+              color: Colors.deepPurpleAccent,
+              fontSize: 26,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
